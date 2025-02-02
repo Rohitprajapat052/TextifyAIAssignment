c0500873fb850598c597722a2637a92a18c562dd
RohitPipalki <114174101+Rohitprajapat052@users.noreply.github.com>
2025-02-02 18:18:42 +0530

    Update SideMenu.tsx

--- src/app/components/side-menu/SideMenu.tsx
+++ src/app/components/side-menu/SideMenu.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation"; // Import usePathname
 
-const SideMenu = ({className = ""}) => {
+const SideMenu = () => {
   const [isExpanded, setIsExpanded] = useState(true);
   const pathname = usePathname(); // Get current route
 
@@ -17,7 +17,7 @@ const SideMenu = ({className = ""}) => {
     >
       <button
         className="mb-6 p-2 rounded-lg bg-gray-100 hover:bg-gray-200 transition"
-        onClick={() => setIsExpanded(!isExpanded)}
+        }
       >
         {isExpanded ? (
           <ChevronLeft className="w-6 h-6 text-gray-600" />
